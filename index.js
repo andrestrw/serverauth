806e4e574c11f60a0c41122e9401e5955d00fb61
andrestrw <133chicagobulls@gmail.com>
2024-03-11 22:14:47 +0100

    ADD First v

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+dotenv.config();
+import { UserRouter } from "./routes/user.js";
+
+const URL_CONNECT2 = process.env.URL_CONNECT2;
+
+const app = express();
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173/",
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+app.use("/auth", UserRouter);
+
+mongoose.connect(URL_CONNECT2);
+
+app.listen(process.env.PORT, () => {
+  console.log("Server is running");
+});
