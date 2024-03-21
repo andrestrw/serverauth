d8e16938f084bee9d4f902a29f77c83979e023cc
andrestrw <133chicagobulls@gmail.com>
2024-03-21 20:17:29 +0100

    despliegue1

--- index.js
+++ index.js
@@ -7,21 +7,23 @@ dotenv.config();
 import { UserRouter } from "./routes/user.js";
 
 const URL_CONNECT2 = process.env.URL_CONNECT2;
+const PORT = process.env.PORT;
 
 const app = express();
 
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173/",
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
+
 app.use(cookieParser());
 app.use("/auth", UserRouter);
 
 mongoose.connect(URL_CONNECT2);
 
-app.listen(process.env.PORT, () => {
+app.listen(PORT, () => {
   console.log("Server is running");
 });
